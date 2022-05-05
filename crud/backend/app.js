35c6a61055b26e8b92519195c7672b145bae6024
Luiz Lopes <luizlopes12@outlook.com>
2022-05-05 12:59:23 -0300

    Iniciando backend

---
+++ crud/backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3001
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
